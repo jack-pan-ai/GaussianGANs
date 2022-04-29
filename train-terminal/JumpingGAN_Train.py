4c8157952a8fa8d82435669edf9fd29f09eaa234
jack-pan-ai <972946551@qq.com>
2022-04-29 15:17:54 +0300

    v3 finished

--- train-terminal/JumpingGAN_Train.py
+++ train-terminal/JumpingGAN_Train.py
@@ -18,12 +18,12 @@ os.system(f"CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ../train_GAN.py \
 --d_lr 0.0003 \
 --loss lsgan \
 --n_dis 1 \
---n_gen 1 \
+--n_gen 5 \
 --patch_size 15 \
 --phi 1 \
 --epochs 3000 \
 --batch_size 16 \
 --print_freq 50 \
---checkpoint_best_PATH 'logs/JumpingInverse-dis-v3_2022_04_29_03_19_20/Model/checkpoint_best_det' \
+--checkpoint_best_PATH 'logs/JumpingInverse-dis-v3_2022_04_29_04_54_02/Model/checkpoint_best_dis' \
 --class_name Jumping \
---exp_name Jumping-dis-v2")
+--exp_name Jumping-dis-v3")
--- train-terminal/RunningGAN_Train.py
+++ train-terminal/RunningGAN_Train.py
@@ -18,12 +18,12 @@ os.system(f"CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ../train_GAN.py \
 --d_lr 0.0003 \
 --loss lsgan \
 --n_dis 1 \
---n_gen 1 \
+--n_gen 5 \
 --patch_size 15 \
 --phi 1 \
 --batch_size 32 \
 --epochs 3000 \
 --print_freq 50 \
---checkpoint_best_PATH 'logs/RunningInverse-dis-v3_2022_04_29_03_19_44/Model/checkpoint' \
+--checkpoint_best_PATH 'logs/RunningInverse-dis-v3_2022_04_29_04_54_18/Model/checkpoint_best_dis' \
 --class_name Running \
---exp_name Running-dis-v2")
+--exp_name Running-dis-v3")
